4916625e365044ffb94955031151e490ca79dc3e
Marek S. Lukasiewicz <marek@lukasiewicz.tech>
2024-08-16 21:33:06 +0200

    Separate commands on serial

--- garden_watering/garden_watering.ino
+++ garden_watering/garden_watering.ino
@@ -1,148 +1,107 @@
-/****************************************************************************/	
-
-//	Function: Set time and get the time from RTC chip(DS1307) and display 
-//			  it on the serial monitor.
-//	Hardware: Grove - RTC
-//	Arduino IDE: Arduino-1.0
-//	Author:	 FrankieChu		
-//	Date: 	 Jan 19,2013
-//	Version: v1.0
-//	by www.seeedstudio.com
-//
-//  This library is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU Lesser General Public
-//  License as published by the Free Software Foundation; either
-//  version 2.1 of the License, or (at your option) any later version.
-//
-//  This library is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//  Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public
-//  License along with this library; if not, write to the Free Software
-//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-//
-/****************************************************************************/
 #include <Wire.h>
 #include "DS1307.h"
 
-DS1307 clock;//define a object of DS1307 class
-const byte numChars = 32;
-char receivedChars[numChars];
-char tempChars[numChars];        // temporary array for use when parsing
+DS1307 clock;  //define a object of DS1307 class
 
-      // variables to hold the parsed data
-char messageFromPC[numChars] = {0};
-int integerFromPC = 0;
-float floatFromPC = 0.0;
+//now the variables to store the data
+DS1307 watering_t1;         //watering time as an object of DS1307 class
+DS1307 watering_t2;         //watering time as an object of DS1307 class
+int watering_thresh = 0;    //percentage for humidity percentage above which watering will not be triggered
+int watering_duration = 0;  //how long to water it for
+int humidity_sensor = 0;    //this variable will store the feedback from the humidity sensor
 
-boolean newData = false;
+const int maxCommandLength = 32;
+char commandText[maxCommandLength] = { 0 };
 
-//now the variables to store the data 
-DS1307 watering_t1; //watering time as an object of DS1307 class 
-DS1307 watering_t2; //watering time as an object of DS1307 class
-int watering_thresh =0; //percentage for humidity percentage above which watering will not be triggered
-int watering_duration =0; //how long to water it for
-int humidity_sensor = 0; //this variable will store the feedback from the humidity sensor
+bool receivingCommand = false;
+bool commandReady = false;
+int commandLength = 0;
 
-//============
-void setup()
-{
-	Serial.begin(9600);
+void setup() {
+  Serial.begin(9600);
+  clock.begin();  // setup connection to device
+
   Serial.println("Please insert the instructions for configuration of the controller.");
-  Serial.println("Enter data in this style command YYMMDD. When done, terminate the command by pressing newline \n");
-  Serial.println();
-	//clock.begin(); figure this out
+  Serial.println("Commands start with '/' and end with newline");
 }
-void loop()
-{ //store the info, is done once per setup
-  recvWithStartEndMarkers();
-  if (newData == true) {
-      strcpy(tempChars, receivedChars);
-          // this temporary copy is necessary to protect the original data
-          //   because strtok() used in parseData() replaces the commas with \0
-      parseData(); //inside here, switch case to differentiate among instructions and save the info in the variables
-      showParsedData();
-      newData = false;
-} //loop that goes on forever until power shuts off
-//every time the hour on the clock corresponds to the one to water
-//run a check on humidity and based on it trigger the water 
-//run code for running water
-//save into memory info regarding this run
-//somewhere add continous check of state pump
+void loop() {
+  // loop that goes on forever until power shuts off
+  receiveCommand();
+  if (commandReady) {
+    Serial.print("Received command: ");
+    Serial.println(commandText);
+    commandReady = false;
+  }
+
+  //every time the hour on the clock corresponds to the one to water
+  //run a check on humidity and based on it trigger the water
+  //run code for running water
+  //save into memory info regarding this run
+  //somewhere add continous check of state pump
 }
 
-
-//============
-
-void parseData() {      // split the data into its parts
-
-    char * strtokIndx; // this is used by strtok() as an index
-
-    strtokIndx = strtok(tempChars," ");      // get the first part - the string
-    strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
- 
-    strtokIndx = strtok(NULL, " "); // this continues where the previous call left off
-    integerFromPC = atoi(strtokIndx);     // convert this part to an integer
-
-    strtokIndx = strtok(NULL, " ");
-    floatFromPC = atof(strtokIndx);     // convert this part to a float
-
-}
-
-//============
-
-void showParsedData() {
-    Serial.print("Message ");
-    Serial.println(messageFromPC);
-    Serial.print("Integer ");
-    Serial.println(integerFromPC);
-    Serial.print("Float ");
-    Serial.println(floatFromPC);
+void receiveCommand() {
+  while (!commandReady && Serial.available() > 0) {
+    char next = Serial.read();
+    if (next == '/') {
+      // start a new command, even if currently receiving one
+      receivingCommand = true;
+      commandLength = 0;
+      commandText[commandLength++] = next;
+    } else if (receivingCommand) {
+      if (next == '\n' || commandLength == maxCommandLength - 1) {
+        // finish reading this command
+        commandText[commandLength] = 0;
+        receivingCommand = false;
+        commandReady = true;
+      } else {
+        // just add this character
+        commandText[commandLength++] = next;
+      }
+    }
+  }
 }
 
 /*Function: Display time on the serial monitor*/
-void printTime()
-{
-	clock.getTime();
-	Serial.print(clock.hour, DEC);
-	Serial.print(":");
-	Serial.print(clock.minute, DEC);
-	Serial.print(":");
-	Serial.print(clock.second, DEC);
-	Serial.print("	");
-	Serial.print(clock.month, DEC);
-	Serial.print("/");
-	Serial.print(clock.dayOfMonth, DEC);
-	Serial.print("/");
-	Serial.print(clock.year+2000, DEC);
-	Serial.print(" ");
-	Serial.print(clock.dayOfMonth);
-	Serial.print("*");
-	switch (clock.dayOfWeek)// Friendly printout the weekday
-	{
-		case MON:
-		  Serial.print("MON");
-		  break;
-		case TUE:
-		  Serial.print("TUE");
-		  break;
-		case WED:
-		  Serial.print("WED");
-		  break;
-		case THU:
-		  Serial.print("THU");
-		  break;
-		case FRI:
-		  Serial.print("FRI");
-		  break;
-		case SAT:
-		  Serial.print("SAT");
-		  break;
-		case SUN:
-		  Serial.print("SUN");
-		  break;
-	}
-	Serial.println(" ");
+void printTime() {
+  clock.getTime();
+  Serial.print(clock.hour, DEC);
+  Serial.print(":");
+  Serial.print(clock.minute, DEC);
+  Serial.print(":");
+  Serial.print(clock.second, DEC);
+  Serial.print("	");
+  Serial.print(clock.month, DEC);
+  Serial.print("/");
+  Serial.print(clock.dayOfMonth, DEC);
+  Serial.print("/");
+  Serial.print(clock.year + 2000, DEC);
+  Serial.print(" ");
+  Serial.print(clock.dayOfMonth);
+  Serial.print("*");
+  switch (clock.dayOfWeek)  // Friendly printout the weekday
+  {
+    case MON:
+      Serial.print("MON");
+      break;
+    case TUE:
+      Serial.print("TUE");
+      break;
+    case WED:
+      Serial.print("WED");
+      break;
+    case THU:
+      Serial.print("THU");
+      break;
+    case FRI:
+      Serial.print("FRI");
+      break;
+    case SAT:
+      Serial.print("SAT");
+      break;
+    case SUN:
+      Serial.print("SUN");
+      break;
+  }
+  Serial.println(" ");
 }
